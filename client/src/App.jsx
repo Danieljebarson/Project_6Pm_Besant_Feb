8492df01ea28d679c34c3b53de59a7fb309fa2b3
Dinesh107 <dineshjr107@gmail.com>
2026-02-20 19:21:41 +0530

    project 20-02-2026  commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import "./App.css";
 function App() {
  const [users, setUsers] = useState([]);
  const [filteredUsers, setfilteredUsers] = useState([]);
+ const [isFormOpen, setIsFormOpen] = useState(false);
+ const [userData, setUserData] = useState({name:"", age: "", city: ""});
+
 
    const getAllUsers = async () => {
     await axios.get("http://localhost:8000/users").then
@@ -32,14 +35,72 @@ const handleSearchChange = (e) => {
      setfilteredUsers(filteredUsers);
 };
 
-    
+// delete function
+
+const handleDelete = async (id) => {
+     const isConfirmed = window.confirm("Are you sure want to delete this user")
+     if(isConfirmed) {
+       await axios.delete(`http://localhost:8000/users/${id}`).then((res) => {
+       setUsers(res.data); // to display all the users when the broswer loaded
+      setfilteredUsers(res.data); // this will show when the user enter the data in serach filed
+     })
+     }
+} 
+
+
+// Add User details 
+const handleAddRecord = () => {
+    setUserData({name:"", age: "", city: ""});
+    setIsFormOpen(true);
+
+}
+
+// close form 
+
+const closeForm = () => {
+  setIsFormOpen(false);
+  getAllUsers(); // 
+}
+
+
+// handleData
+
+
+const handleData = (e) => {
+   setUserData({...userData, [e.target.name] : e.target.value})
+}
+
+// handleUpdateData
+
+const handleUpdateData = (user) => {
+  setUserData(user);
+   setIsFormOpen(true);
+}
+
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  console.log(userData); // CHECK THIS FIRST
+
+  try {
+    const res = await axios.post("http://localhost:8000/users", userData);
+     alert("new user added succecfully"); 
+    console.log(res.data);
+    closeForm(); // close the form after send the data
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
   return (
     <>
       <div className="container">
         <h3>Crud Project with Reactjs and Node as Backend</h3>
         <div className="input-search">
           <input type="search" placeholder="search text here" onChange={handleSearchChange} />
-          <button className="btn green" >Add New User</button>
+          <button className="btn green" onClick={handleAddRecord}  >Add New User</button>
           <table className="table">
             <thead>
               <tr>
@@ -65,10 +126,10 @@ const handleSearchChange = (e) => {
                 
                 <td>{user.city}</td>
                 <td>
-                  <button className="btn green" >Edit</button>
+                  <button className="btn green" onClick={()=>handleUpdateData(user)} >Edit</button>
                 </td>
                 <td>
-                  <button className="btn red" >Delete</button>
+                  <button onClick={() => handleDelete(user.id)} className="btn red" >Delete</button>
                 </td>
               </tr>
                   )
@@ -77,6 +138,27 @@ const handleSearchChange = (e) => {
              
             </tbody>
           </table>
+          {isFormOpen && (
+               <div className="form" >
+                <div className="form-content" >
+                  <span className="close" onClick={closeForm}>&times;</span>
+                  <h2>User Record</h2> 
+                  <div className="input-group">
+                    <label htmlFor="name">Full name</label>
+                    <input type="text" value={userData.name} onChange={handleData} name="name" id="name" />
+                  </div>
+                  <div className="input-group">
+                    <label htmlFor="age">Age</label>
+                    <input type="number" value={userData.age} onChange={handleData} name="age" id="age" />
+                  </div>
+                  <div className="input-group">
+                    <label htmlFor="city">City</label>
+                    <input type="text"  value={userData.city} onChange={handleData} name="city" id="city" />
+                  </div>
+                  <button type="button" className="btn green" onClick={handleSubmit} >Add user</button>
+                </div>
+               </div>
+          )}
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -2,20 +2,50 @@ const express = require("express");
 const users = require("./sample.json");
 const app = express();
 const cors = require("cors");
-
+const fs = require("fs");
+app.use(express.json());
 
 const port = 8000;
 app.use(cors()); // middleware - to cors issue
 
-// display all users 
+// display all users
 app.get("/users", (request, response) => {
-    return response.json(users);
-})
-
-
-
-
-app.listen(port, (err) => {
-    console.log(`App is started, running in the ${port}`);
+  return response.json(users);
 });
 
+// Delete user - creating the route in server
+
+//                  "1"
+app.delete("/users/:id", (req, res) => {
+  //                  7
+  let id = Number(req.params.id);
+  //                                         1       7
+  let filterUsers = users.filter((user) => user.id != id);
+
+  // database we are re writing
+  fs.writeFile("./sample.json", JSON.stringify(filterUsers), (err, data) => {
+    return res.json(filterUsers);
+  });
+});
+
+// add new user
+app.post("/users", (req, res) => {
+  let { name, age, city } = req.body;  // object destructuring 
+
+  if (!name || !age || !city) {
+    return res.status(400).send({ message: "all field required" });
+  }
+
+  const users = JSON.parse(fs.readFileSync("./sample.json")); // users varailbe la sample.json data store agi errukum
+
+  let id = Date.now(); // to genrate the ID in random order
+  users.push({ id, name, age, city });
+
+  fs.writeFile("./sample.json", JSON.stringify(users), (err) => {
+    return res.json(users);
+  });
+});
+
+app.listen(port, (err) => {
+  console.log(`App is started, running in the ${port}`);
+});
